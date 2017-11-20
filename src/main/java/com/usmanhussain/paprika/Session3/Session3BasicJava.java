8a9a16ea1a4feee671e2b89a95536261b6f9afd3
hemanth.kuppuswamynaidu <hemachandrarao.kuppuswamynaidu@directlinegroup.co.uk>
2017-11-20 16:00:40 +0000

    Hemanth - Exception training snippets

--- src/main/java/com/usmanhussain/paprika/Session3/Session3BasicJava.java
+++ src/main/java/com/usmanhussain/paprika/Session3/Session3BasicJava.java
@@ -1,27 +1,26 @@
 package com.usmanhussain.paprika.Session3;
 
+import java.io.Reader;
+import java.util.Scanner;
+
 public class Session3BasicJava {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Simply printing "Hello World"
         System.out.println("Hello World");
 
         //creating an object
         Session3BasicJavaClass2 objClass = new Session3BasicJavaClass2("test");
+//        try {
+            System.out.println("enter an integer");
+            Scanner scanner = new Scanner(System.in);
+            int number = scanner.nextInt();
+            objClass.printNamesWithIndex(number);
+       // } catch (Throwable e) {
+            System.out.println(" you have enterd an invalid number ");
+       // }
 
-        //Calling different methods defined in Session3BasicJavaClass2 class
-        //When a method is called, based on the type of method either pass the required parameters of correct
-        //data types or simple call the method(if no input parameter defined for the method)
-        objClass.printDynamicMessage("testing");
-        objClass.printStaticMessage();
-        objClass.printProtectedMessage();
-        objClass.printBooleanVariable(false);
-        objClass.printID(1);
-        objClass.printNameAsString(1);
-        objClass.printNameAsString(5);
-        objClass.printNameWithSwitch(5);
-        objClass.printNamesWithIndex(1);
-        objClass.printConcatenateString("testing","test");
+        objClass.printConcatenateString("testing", "test");
     }
 
 }
