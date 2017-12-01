468263b3533f7b92a0148042df07e919a033840f
PreetamUppunda <preetam.uppunda@directlinegroup.co.uk>
2017-12-01 18:11:12 +0000

    Preetam - added to receive error in feature

--- src/test/java/com/usmanhussain/paprika/step_definitions/SmokeTest.java
+++ src/test/java/com/usmanhussain/paprika/step_definitions/SmokeTest.java
@@ -10,7 +10,7 @@ public class SmokeTest extends BaseStepDef {
 
     @Given("^I have navigated to a website$")
     public void iHaveNavigatedToAWebsite() throws Throwable {
-        getDriver().get("http://www.bing.com");
+        //getDriver().get("http://www.bing.com");
     }
 
     @When("^I search for the following '(.+)'$")
