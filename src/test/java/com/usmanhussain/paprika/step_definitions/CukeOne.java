dd9f03fde48c233e53aa52bb03470f1d8e1cd802
uhussain <Ibrahim1>
2018-02-06 13:19:16 +0000

    Usman - Checking in example from the training session on the 6th Feb on Cucumber

---
+++ src/test/java/com/usmanhussain/paprika/step_definitions/CukeOne.java
@@ -0,0 +1,79 @@
+package com.usmanhussain.paprika.step_definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by usman on 06/02/2018.
+ */
+public class CukeOne {
+
+    @Given("^I am on the login page$")
+    public void i_am_on_the_login_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^i successfully login$")
+    public void i_successfully_login() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^i should see the next page$")
+    public void i_should_see_the_next_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+//    ----------------------------------------------------------------------------------------
+
+    @Given("^i am on google$")
+    public void i_am_on_google() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^i search for (.+)$")
+    public void iSearchForTransportation(String var1) throws Throwable {
+
+
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I see the correct cars results$")
+    public void i_see_the_correct_cars_results() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+//    @When("^i search for trains$")
+//    public void i_search_for_trains() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new PendingException();
+//    }
+
+    @Then("^I see the correct trains results$")
+    public void i_see_the_correct_trains_results() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+//    @When("^i search for planes$")
+//    public void i_search_for_planes() throws Throwable {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new PendingException();
+//    }
+
+    @Then("^I see the correct planes results$")
+    public void i_see_the_correct_planes_results() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+}
--- src/test/resources/features/OOP.feature
+++ src/test/resources/features/OOP.feature
@@ -12,3 +12,25 @@ Feature: This is the feature file for the session on Design Patterns
     Given I am on the Direct Line homepage
     When I click on the motor quote button
     Then I can verify all the fields are present
+
+  @usman1
+  Scenario: Login test 1
+    Given I am on the login page
+    When I am on the login page
+    When i successfully login
+    Then i should see the next page
+
+  @usman2
+  Scenario: Login test 2
+    Given I am on the login page
+    And I enter in the username
+    And i enter in the password
+    When i click on the login button
+    Then i should be logged in
+
+  @usman3
+  Scenario: Login test 3
+    Given I am on the login page
+    When blah blah blhab
+    Then I should be taken to the 'About You' page
+
